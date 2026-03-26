06d2fa7ee6fa620004801c488cfbe6a5dcbd499a
Bojan Stefanovski <bojanpece0@gmail.com>
2026-03-26 18:54:24 +0100

    Initial Commit

---
+++ MA_Lab/MA_LabApp.swift
@@ -0,0 +1,17 @@
+//
+//  MA_LabApp.swift
+//  MA_Lab
+//
+//  Created by Bojan Stefanovski on 26/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct MA_LabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
